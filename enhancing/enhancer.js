cffb84f4a71f7e8576efa9005187de3d68064a1f
focused220 <roblhjr.220@gmail.com>
2019-06-27 18:34:08 -0700

    set up dependencies

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,15 +6,23 @@ module.exports = {
 };
 
 function succeed(item) {
-  return { ...item };
+  let enhancedItem = item;
+  if(enhancedItem.enhancement < 20){return enhancedItem.enhancement + 1;}
+  else {return enhancedItem.enhancement}
 }
 
 function fail(item) {
-  return { ...item };
+  let newItem = item;
+  if(newItem.enhancement < 15){ newItem.durability = newItem.durability - 5; return newItem}
+  else if(newItem.enhancement >= 15){newItem.durability - 10; return newItem}
+  else if(newItem.enhancement > 16){newItem.enhancement - 1; return newItem}
 }
 
 function repair(item) {
-  return { ...item };
+  let newItem = item;
+  newItem.durability = 100;
+  let durability = newItem.durability;
+  return  durability;
 }
 
 function get(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,44 @@
-const enhancer = require('./enhancer.js');
+const {repair, succeed, fail} = require('./enhancer.js');
 // test away!
+
+describe('enhancer functioms', () => {
+    it('repair should restore the durability to 100', () =>{
+        const item = {
+            name: 'item1',
+            durability: 50,
+            enhancement: 5
+        };
+        const repaired = repair(item);
+
+        expect(repaired).toBe(100);
+    })
+    it('succeed should increase enhancement by 1', () => {
+
+        const item = {
+            name: 'item1',
+            durability: 50,
+            enhancement: 5
+        };
+
+        const enhanced = succeed(item);
+        expect(enhanced).toBe(6)
+    })
+    it('failed should change durability based on enhancement', () => {
+
+        const item = {
+            name: 'item1',
+            durability: 50,
+            enhancement: 5
+        };
+        
+        const failed = fail(item);
+        const result = {
+            name: 'item1',
+            durability: 45,
+            enhancement: 5
+        }
+        expect(failed).toEqual(result)
+    })
+});
+
+
